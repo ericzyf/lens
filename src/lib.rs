5a8b9adc065395b395e5a202dfc1a855c549a9f3
Yifan Zhang <yfzhang1999@gmail.com>
2022-11-15 22:06:54 +0800

    4.1

--- src/lib.rs
+++ src/lib.rs
@@ -2,3 +2,30 @@ pub mod vec3;
 pub use vec3::Vec3;
 pub mod point3;
 pub use point3::Point3;
+
+#[derive(Debug, Copy, Clone)]
+pub struct Ray {
+    orig: Point3,
+    dir: Vec3,
+}
+
+impl Ray {
+    pub fn new(origin: Point3, direction: Vec3) -> Self {
+        Ray {
+            orig: origin,
+            dir: direction,
+        }
+    }
+
+    pub fn origin(&self) -> Point3 {
+        self.orig
+    }
+
+    pub fn direction(&self) -> Vec3 {
+        self.dir
+    }
+
+    pub fn at(&self, t: f64) -> Point3 {
+        self.origin() + t * self.direction()
+    }
+}
