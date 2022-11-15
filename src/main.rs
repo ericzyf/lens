a8a06760441b5d599ba703d4f149d1d5cd9ccc23
Yifan Zhang <yfzhang1999@gmail.com>
2022-11-16 00:28:54 +0800

    4.2

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,42 @@
+use lens::*;
+
+fn ray_color(r: &Ray) -> Color {
+    let unit_direction = r.direction().normalize();
+    let t = 0.5 * (unit_direction.y() + 1.);
+    (1. - t) * Color::new(1., 1., 1.) + t * Color::new(0.5, 0.7, 1.)
+}
+
 fn main() {
     // Image
-    let image_width = 256;
-    let image_height = 256;
+    let aspect_ratio = 16. / 9.;
+    let image_width = 1280;
+    let image_height = ((image_width as f64) / aspect_ratio) as u32;
+
+    // Camera
+    let viewport_height = 2.;
+    let viewport_width = aspect_ratio * viewport_height;
+    let focal_length = 1.;
+
+    let origin = Point3::new(0., 0., 0.);
+    let horizontal = Vec3::new(viewport_width, 0., 0.);
+    let vertical = Vec3::new(0., viewport_height, 0.);
+    let ll_corner = origin - horizontal / 2. - vertical / 2. - Vec3::new(0., 0., focal_length);
 
     let mut imgbuf = image::ImageBuffer::new(image_width, image_height);
 
     // Render
     for (x, y, pixel) in imgbuf.enumerate_pixels_mut() {
-        let r = (x as f64) / ((image_width as f64) - 1.0);
-        let g = (y as f64) / ((image_height as f64) - 1.0);
-        let b = 0.25;
+        let u = (x as f64) / ((image_width as f64) - 1.);
+        let v = (y as f64) / ((image_height as f64) - 1.);
 
-        let ir = (255.999 * r) as u8;
-        let ig = (255.999 * g) as u8;
-        let ib = (255.999 * b) as u8;
-        *pixel = image::Rgb([ir, ig, ib]);
+        *pixel = image::Rgb::from(ray_color(&Ray::new(
+            origin,
+            ll_corner + u * horizontal + v * vertical - origin,
+        )));
     }
 
-    image::imageops::flip_vertical(&imgbuf).save("image.png").unwrap();
+    image::imageops::flip_vertical(&imgbuf)
+        .save("image.png")
+        .unwrap();
     println!("Done");
 }
