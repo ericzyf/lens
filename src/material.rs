a8341c38ad88e33e5bb9401c0f987ac5e6f45222
Yifan Zhang <yfzhang1999@gmail.com>
2022-11-19 16:13:00 +0800

    enum Material

--- src/material.rs
+++ src/material.rs
@@ -3,3 +3,8 @@ use super::{Color, HitRecord, Ray};
 pub trait Scatterable {
     fn scatter(r_in: &Ray, rec: &HitRecord) -> Option<(Color, Ray)>;
 }
+
+#[derive(Debug, Copy, Clone)]
+pub enum Material {
+    Lambertian,
+}
